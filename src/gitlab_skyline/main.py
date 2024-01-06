24fa711d0340ad7ac9068c923fc02d9913591f14
ikaruswill <ikaruswill.ho@gmail.com>
2024-01-06 16:15:08 +0800

    Fix int str conversion in new templated filename

--- src/gitlab_skyline/main.py
+++ src/gitlab_skyline/main.py
@@ -56,7 +56,7 @@ def get_userid(username: str, gitlab_url) -> int:
 def get_output_filename(url: str, username: str, year: int):
     domain = urllib.parse.urlparse(url).netloc
     separator = "_"
-    return separator.join((domain, username, year))
+    return separator.join((domain, username, str(year)))
 
 
 def put_contributions_cache(date_contributions: List[Tuple[datetime.date, int]], path: Path):
